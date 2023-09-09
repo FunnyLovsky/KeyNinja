51a6d410d0f7e65ea09563a2817d3dd580b4ff58
Funny_lovsky <crazy.ted.2000@mail.ru>
2023-09-09 17:41:18 +0300

    change: webpack config

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ["@babel/polyfill", './src/index.jsx'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(css|less)$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(jpeg|jpg|png|svg|wav|mp3)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.m?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({template: './public/index.html'}),
+        new CleanWebpackPlugin()
+    ]
+}
